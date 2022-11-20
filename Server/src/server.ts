9c350ced287c2af75b20663ca8d9641a2a665ea8
Centurion <centuriononon@gmail.com>
2022-11-20 20:51:12 +0400

    [starter] the start script was prescribed and tested

---
+++ Server/src/server.ts
@@ -0,0 +1,21 @@
+// Require the framework and instantiate it
+import fastify from 'fastify';
+
+const app = fastify({ logger: true });
+
+// Declare a route
+app.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+const run = async () => {
+  try {
+    await app.listen({ port: 3000 })
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+
+run();
